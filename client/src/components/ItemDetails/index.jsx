4e6ce325d7b98b4530a679262690db371e491c02
Evan OShea <evanoshea21@gmail.com>
2023-01-13 16:45:15 -0800

    Added index filed for components

---
+++ client/src/components/ItemDetails/index.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+
+const ItemDetails = () => {
+  return (
+    <div>ItemDetails
+
+
+    </div>
+  )
+};
+
+export default ItemDetails;
---
+++ client/src/components/ProfilePage/index.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+
+const ProfilePage = () => {
+  return (
+    <div>ProfilePage
+
+
+    </div>
+  )
+};
+
+export default ProfilePage;
