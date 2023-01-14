56f120ca41550604c3e16068b4452af8e6ef6ec3
Bo Shao <boshao13@gmail.com>
2023-01-13 18:00:29 -0700

    some initial wireframe

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Profile from './ProfilePage/Profile';
 
 export default function App() {
   return (
-    <div>Hello, world!</div>
+    <>
+      <div>Hello, world!</div>
+      <Profile />
+    </>
+
   );
 }
---
+++ client/src/components/ProfilePage/PendingTrades.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Box } from '@mui/material/';
+
+function PendingTrades() {
+  return (
+    <Box sx={{ bgcolor: '#ff9966', height: '20vh' }}>
+      pending tradess
+    </Box>
+  );
+}
+export default PendingTrades;
