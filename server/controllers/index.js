a04bc89af64b9b401b8da6d854b55a040141185c
Evan OShea <evanoshea21@gmail.com>
2023-01-14 17:43:37 -0800

    evans updates

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -6,71 +6,21 @@ module.exports.Trades = require('./Trades');
 
 module.exports.Shared = {
 
-  getItemFromID: function(req,res) {
+  getItemFromID: async function(req,res) {
     const itemID = req.params.itemID;
-    /* Get relevant data from req
-
-
-    models.getItemFromID(data)
-    .(res => {
-
-  } */
-
-  },
-
-  /*
-  var getConnection = function(callback) {
-    pool.getConnection(function(err, connection) {
-        callback(err, connection);
-    });
-};
-
-module.exports = getConnection;
-  */
-  getUser: async function(req,res) {
-    const qString = `SELECT * FROM users WHERE id = ${req.params.userID}`;
+    console.log('ITEM ID', itemID);
+    const qString = `SELECT * FROM devices WHERE id = ${itemID}`;
 
     try {
       const connection = await db.getConnection();
       const results = await connection.query(qString);
-      const [[sendBack]]= results;
-      console.log('Results in GETUSER-> ', sendBack);
+      const [sendBack]= results;
+      console.log('Results in getItems-> ', sendBack);
       res.status(200).send(sendBack);
 
     } catch {
       res.status(500).send('error')
     }
-
-    // const userID = req.params.userID;
-    // console.log('userid', userID);
-    // // console.log(db.query);
-    // db.pool1.getConnection(function(err, connection) {
-    //   // callback(err, connection);
-    //   connection.query( //or CONNECTION.query
-    //     'SELECT * FROM users WHERE id = ?',
-    //     [userID],
-    //     function(err, results, fields) {
-    //       if(err) {
-    //         console.error('error in getUser controller', err);
-    //         res.status(500).send(err);
-    //       }
-    //       res.status(200).send(results);
-    //     }
-    //   )//end query
-
-      // db.query( //or CONNECTION.query
-      //   'SELECT * FROM users WHERE id = ?',
-      //   [userID],
-      //   function(err, results, fields) {
-      //     if(err) {
-      //       console.error('error in getUser controller', err);
-      //       res.status(500).send(err);
-      //     }
-      //     res.status(200).send(results);
-      //   }
-      // )//end query
-
-
 
   },
 
--- server/routes.js
+++ server/routes.js
@@ -5,8 +5,6 @@ router.get('/users/auth', Users.authUser);
 router.get('/users/user/:id', Users.getUser);
 router.post('/users', Users.createUser);
 
-router.get('/user/:userID', Shared.getUser);//
-
 router.post('/get/trades/involved', Trades.getInvolvedTrades);//
 router.get('/item/:itemID', Shared.getItemFromID);//
 router.get('/trade/status/:tradeID/:newStatus', Trades.updateTradeFromID);
