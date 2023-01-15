7b9136b61c6cefa811e0d954dd5d171180bf099c
Jordan Searcy-Hosea <jkshosea@gmail.com>
2023-01-14 15:29:20 -1000

    Add route

--- server/routes.js
+++ server/routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { Users } = require('./controllers');
 
 router.get('/users/auth', Users.authUser);
+router.get('/users/user/:id', Users.getUser);
 router.post('/users', Users.createUser);
 
 module.exports = router;
