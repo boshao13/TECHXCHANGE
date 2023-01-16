cf1baa9366af6ae342287e7f18c8cd858d30af59
Erin Antoine <erin.antoine1@gmail.com>
2023-01-15 18:01:26 -0800

    added getItemFromUserID route/controller/API function

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -9,18 +9,36 @@ module.exports.Shared = {
   getItemFromID: async function(req,res) {
     const itemID = req.params.itemID;
     const qString = `SELECT * FROM devices WHERE id = ${itemID}`;
-
+    console.log(itemID);
     try {
       const connection = await db.getConnection();
       const results = await connection.query(qString);
       const [[sendBack]] = results;
-      res.status(200).send(sendBack);
-
-    } catch {
-      res.status(500).send('error')
+      res.status(200).json(sendBack);
+    } catch (error) {
+      console.log(error);
+      res.status(500).send('error:', error)
     }
 
   },
+  getItemFromUserID: async function(req,res) {
+    const userID = req.params.userID;
+    const qString = `SELECT * FROM devices WHERE user_id = ${userID}`;
+    console.log(userID);
+    try {
+      const connection = await db.getConnection();
+      const results = await connection.query(qString);
+
+      const [sendBack] = results;
+      console.log(sendBack);
+      res.status(200).json(sendBack);
+    } catch (error) {
+      console.log(error);
+      res.status(500).send('error:', error)
+    }
+
+  },
+
 
 
 
