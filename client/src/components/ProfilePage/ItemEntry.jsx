fe9906303aee4c16b8715643f306571b1e55d674
Bo Shao <boshao13@gmail.com>
2023-01-16 17:32:42 -0700

    alot of css

--- client/src/components/ProfilePage/ItemEntry.jsx
+++ client/src/components/ProfilePage/ItemEntry.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import { Box, Avatar } from '@mui/material/';
 
+const avatarSX = {
+  marginBottom:'10px',
+  boxShadow: `-5px -5px 10px rgba(232,242,255,0.8),
+  5px 5px 10px rgba(0,0,0,0.25)`,
+  marginTop: '15px',
+  width: '200px',
+  height: '200px'
+};
+
 function ItemEntry() {
   return (
     <Box sx={{
@@ -8,18 +17,17 @@ function ItemEntry() {
       justifyContent: 'center',
       alignContent: 'center',
       display: 'flex',
-      bgcolor: '#269891',
+      bgcolor: '#CAF0F8',
       height: '10vh',
     }}
     >
 
       <Box sx={{
         display: 'flex', flexWrap: 'wrap', justifyContent: 'center', alignContent: 'center',
-      }}
-      >
+      }}>
         <Avatar />
       </Box>
-      <Box sx={{ flexGrow: 1, alignContent: 'center', bgcolor: '#112063' }}>
+      <Box sx={{ flexGrow: 1, alignContent: 'center', bgcolor: '#3466A3' }}>
         <div> Title</div>
         <div> condition </div>
         <div> description</div>
