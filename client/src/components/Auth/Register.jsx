463a9d5b26929b49217ac99948c708a2ce9717bd
Jordan Searcy-Hosea <jkshosea@gmail.com>
2023-01-17 08:16:14 -1000

    Implement Register component

---
+++ client/src/components/Auth/Register.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { Container, Paper, Typography, TextField, Button } from '@mui/material';
+import axios from 'axios';
+
+export default function Register({ props: { setUser, setRegister } }) {
+  const handleSubmit = async event => {
+    event.preventDefault();
+
+    const data = {
+      email: event.target.email.value,
+      password: event.target.password.value,
+      name: event.target.name.value,
+      description: event.target.description.value,
+      street: event.target.street.value,
+      zip_code: event.target.zip_code.value,
+    };
+    const { data: user } = await axios.post('/users', data);
+    if (user) {
+      localStorage.setItem('user', user);
+      setUser(user);
+    }
+  };
+
+  return (
+    <Paper
+      elevation={5}
+      sx={{
+        position: 'fixed',
+        width: '45vw',
+        height: '70vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+      }}
+    >
+      <Typography sx={{ textAlign: 'center', fontSize: '2.25rem', fontWeight: 'bold' }}>
+        TechXchange
+      </Typography>
+      <form onSubmit={handleSubmit} style={{ width: '75%' }}>
+        <Container maxWidth="sm">
+          <TextField type="email" label="Email" name="email" required sx={{ width: '100%' }} />
+          <hr />
+          <TextField type="password" label="Password" name="password" required sx={{ width: '100%' }} />
+          <hr />
+          <TextField type="text" label="Name" name="name" required sx={{ width: '100%' }} />
+          <hr />
+          <TextField type="text" label="Description" name="description" sx={{ width: '100%' }} />
+          <hr />
+          <TextField type="text" label="Street" name="street" required sx={{ width: '100%' }} />
+          <hr />
+          <TextField type="text" label="Zip Code" name="zip_code" required sx={{ width: '100%' }} />
+          <hr />
+          <Button type="submit" variant="contained" sx={{ width: '100%' }}>Register</Button>
+          <Typography
+            onClick={() => setRegister(false)}
+            sx={{
+              color: '#0077B6',
+              fontSize: '.9rem',
+              fontWeight: 'bold',
+              ':hover': { cursor: 'pointer' },
+            }}
+          >
+            Already have an account?
+          </Typography>
+        </Container>
+      </form>
+    </Paper>
+  );
+}
