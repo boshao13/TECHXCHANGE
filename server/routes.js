22373271efed7c98b24f932b7c17b9144b0fc0c5
Evan OShea <evanoshea21@gmail.com>
2023-01-17 17:13:43 -0800

    insertDevice API route made

--- server/routes.js
+++ server/routes.js
@@ -9,6 +9,7 @@ router.post('/trades/involved', Trades.getInvolvedTrades);//WORKING
 router.get('/trade/:tradeID', Trades.getTradeFromID);//..........
 router.get('/item/:itemID', Shared.getItemFromID);//WORKING
 router.get('/items/user/:userID', Shared.getItemsFromUserID); //WORKING
+router.post('/item/insert/:userID', Shared.insertDevice); //WORKING
 router.put('/trade/status/:tradeID/:newStatus', Trades.updateTradeFromID);//WORKING
 router.post('/trades', Trades.createNewTrade); //WORKING
 
