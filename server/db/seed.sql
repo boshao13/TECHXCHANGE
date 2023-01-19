31eaa144876362eb9c7cd43e27309ec88257a514
Kyle Robers <kyl.rober@gmail.com>
2023-01-19 12:54:20 -0700

    Update seed.sql

--- server/db/seed.sql
+++ server/db/seed.sql
@@ -1,8 +1,8 @@
 USE techxchange;
 
--- DELETE FROM users;
--- DELETE FROM devices;
--- DELETE FROM trades;
+DELETE FROM users;
+DELETE FROM devices;
+DELETE FROM trades;
 
 INSERT INTO users (`email`, `name`, `password`, `thumbnail_url`, `description`, `street`, `city`, `state`, `latitude`, `longitude`)
 VALUES ('bob@gmail.com', 'bob', 'pass123', 'https://pyxis.nymag.com/v1/imgs/451/ba2/6f22dfb79768b5c0841c4570cbd8cfb7bf-13-armond-white-2.rsquare.w330.jpg', 'i love trading phones', '1 street way', 'Los Angeles', 'CA', 40.731843, -73.932837);
